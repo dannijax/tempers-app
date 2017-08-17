f3db4600d19ab3209f74155868e1ae9a3c8c92a9
danijax <aliibra19@yahoo.com>
2017-08-17 15:52:31 +0100

    Add other files

---
+++ app/src/test/java/com/tempry/ExampleUnitTest.java
@@ -0,0 +1,23 @@
+package com.tempry;
+
+import org.junit.Test;
+
+import chapters.extension.ExtensionUtilsKt;
+import filehelpers.Sha512;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        //assertEquals(4, 2 + 2);
+
+        assertEquals("fa585d89c851dd338a70dcf535aa2a92fee7836dd6aff1226583e88e0996293f16bc009c652826e0fc5c706695a03cddce372f139eff4d13959da6f1f5d3eabe "
+                , Sha512.getHashCodeFromString("12345678"));
+    }
+}
